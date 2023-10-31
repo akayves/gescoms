36331ecfcfa731dd6d086d0c26b4f41d62075908
akayves <koudou.ay@gmail.com>
2023-10-31 21:41:03 +0100

    Conception du menu

--- resources/views/includes/navleft.blade.php
+++ resources/views/includes/navleft.blade.php
@@ -1,40 +1,143 @@
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
     <ul class="nav">
-    <li class="nav-item">
-        <a class="nav-link" href="index.html">
-        <i class="ti-home menu-icon"></i>
-        <span class="menu-title">Dashboard</span>
-        </a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" data-toggle="collapse" href="#form-elements" aria-expanded="false" aria-controls="form-elements">
-        <i class="ti-clipboard menu-icon"></i>
-        <span class="menu-title">Form elements</span>
-        <i class="menu-arrow"></i>
-        </a>
-        <div class="collapse" id="form-elements">
-        <ul class="nav flex-column sub-menu">
-            <li class="nav-item"><a class="nav-link" href="basic_elements.html">Basic Elements</a></li>
-            <li class="nav-item"><a class="nav-link" href="advanced_elements.html">Advanced Elements</a></li>
-            <li class="nav-item"><a class="nav-link" href="validation.html">Validation</a></li>
-            <li class="nav-item"><a class="nav-link" href="wizard.html">Wizard</a></li>
-        </ul>
-        </div>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" data-toggle="collapse" href="#tables" aria-expanded="false" aria-controls="tables">
-        <i class="ti-layout menu-icon"></i>
-        <span class="menu-title">Tables</span>
-        <i class="menu-arrow"></i>
-        </a>
-        <div class="collapse" id="tables">
-        <ul class="nav flex-column sub-menu">
-            <li class="nav-item"> <a class="nav-link" href="basic-table.html">Basic table</a></li>
-            <li class="nav-item"> <a class="nav-link" href="data-table.html">Data table</a></li>
-            <li class="nav-item"> <a class="nav-link" href="js-grid.html">Js-grid</a></li>
-            <li class="nav-item"> <a class="nav-link" href="sortable-table.html">Sortable table</a></li>
-        </ul>
-        </div>
-    </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+            <i class="ti-home menu-icon"></i>
+            <span class="menu-title">Dashboard</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#fichiers" aria-expanded="false" aria-controls="fichiers">
+            <i class="ti-clipboard menu-icon"></i>
+            <span class="menu-title">Fichiers</span>
+            <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="fichiers">
+            <ul class="nav flex-column sub-menu">
+                <li class="nav-item">
+                    <a class="nav-link" href="basic_elements.html">Rayon</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="advanced_elements.html">Famille</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="validation.html">Sous famille</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="wizard.html">Nature article</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="wizard.html">Magasin</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="wizard.html">Tva</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="wizard.html">Devise</a>
+                </li>
+            </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#achats" aria-expanded="false" aria-controls="achats">
+            <i class="ti-clipboard menu-icon"></i>
+            <span class="menu-title">Achats</span>
+            <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="achats">
+            <ul class="nav flex-column sub-menu">
+                <li class="nav-item"><a class="nav-link" href="">Fournisseurs</a></li>
+                <li class="nav-item"><a class="nav-link" href="">Liste fournisseurs</a></li>
+                <li class="nav-item"><a class="nav-link" href="">Bordereaux ravitaillement</a></li>
+                <li class="nav-item"><a class="nav-link" href="">Liste bordereaux ravitaillement</a></li>
+                <li class="nav-item"><a class="nav-link" href="">Liste détaillé bordereaux</a></li>
+                <li class="nav-item"><a class="nav-link" href="">Saisie facture</a></li>
+                <li class="nav-item"><a class="nav-link" href="">Liste des factures</a></li>
+                <li class="nav-item"><a class="nav-link" href="">Détail des factures</a></li>
+            </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#stocks" aria-expanded="false" aria-controls="stocks">
+            <i class="ti-layout menu-icon"></i>
+            <span class="menu-title">Stocks</span>
+            <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="stocks">
+            <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="basic-table.html">Article</a></li>
+                <li class="nav-item"> <a class="nav-link" href="data-table.html">Liste des articles</a></li>
+                <li class="nav-item"> <a class="nav-link" href="js-grid.html">Inventaire général</a></li>
+                <li class="nav-item"> <a class="nav-link" href="sortable-table.html">cloture d'inventaire</a></li>
+            </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#ventes" aria-expanded="false" aria-controls="ventes">
+            <i class="ti-layout menu-icon"></i>
+            <span class="menu-title">Ventes</span>
+            <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="ventes">
+            <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="basic-table.html">Enregistrement client</a></li>
+                <li class="nav-item"> <a class="nav-link" href="data-table.html">Liste client</a></li>
+                <li class="nav-item"> <a class="nav-link" href="js-grid.html">Bordereaux des commandes</a></li>
+                <li class="nav-item"> <a class="nav-link" href="sortable-table.html">Liste bordereaux des commandes</a></li>
+                <li class="nav-item"> <a class="nav-link" href="js-grid.html">Liste detaillé bordereaux de livraison</a></li>
+                <li class="nav-item"> <a class="nav-link" href="js-grid.html">Inventaire hebdomadaires commerciaux</a></li>
+                <li class="nav-item"> <a class="nav-link" href="sortable-table.html">Iventaire géneral des commerciaux</a></li>
+                <li class="nav-item"> <a class="nav-link" href="js-grid.html">vente directes</a></li>
+                <li class="nav-item"> <a class="nav-link" href="sortable-table.html">Liste des ventes</a></li>
+                <li class="nav-item"> <a class="nav-link" href="sortable-table.html">suivi période des ventes</a></li>
+            </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#depenses-divers" aria-expanded="false" aria-controls="depenses-divers">
+            <i class="ti-layout menu-icon"></i>
+            <span class="menu-title">Dépenses divers</span>
+            <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="depenses-divers">
+            <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="basic-table.html">Saisie des depenses</a></li>
+                <li class="nav-item"> <a class="nav-link" href="data-table.html">Etats détaillé des dépenses</a></li>
+            </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#etats" aria-expanded="false" aria-controls="etats">
+            <i class="ti-layout menu-icon"></i>
+            <span class="menu-title">Etats</span>
+            <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="etats">
+            <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="basic-table.html">Journal des ventes journalier</a></li>
+                <li class="nav-item"> <a class="nav-link" href="data-table.html">Journal des ventes périodiques</a></li>
+                <li class="nav-item"> <a class="nav-link" href="js-grid.html">Journal des ventes regroupés</a></li>
+                <li class="nav-item"> <a class="nav-link" href="sortable-table.html">Classement des clients par C.A</a></li>
+                <li class="nav-item"> <a class="nav-link" href="sortable-table.html">Journal des achats</a></li><li class="nav-item"> <a class="nav-link" href="sortable-table.html">Journal des achats regroupé</a></li>
+                <li class="nav-item"> <a class="nav-link" href="sortable-table.html">Classement des fournisseurs par C.A</a></li>
+                <li class="nav-item"> <a class="nav-link" href="sortable-table.html">Synthèses des opérations</a></li>
+            </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="collapse" href="#parametres" aria-expanded="false" aria-controls="parametres">
+            <i class="ti-layout menu-icon"></i>
+            <span class="menu-title">Parametres</span>
+            <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="parametres">
+            <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="basic-table.html">Basic table</a></li>
+                <li class="nav-item"> <a class="nav-link" href="data-table.html">Data table</a></li>
+                <li class="nav-item"> <a class="nav-link" href="js-grid.html">Js-grid</a></li>
+                <li class="nav-item"> <a class="nav-link" href="sortable-table.html">Sortable table</a></li>
+            </ul>
+            </div>
+        </li>
     </ul>
 </nav>
